b7a65cd5bc49f0450f87d90111a94dba4921e1f5
Valleyard <50876390+Valleyard@users.noreply.github.com>
2020-07-02 14:42:53 +0800

    Add files via upload

--- react-daoyun/src/utils/request.js
+++ react-daoyun/src/utils/request.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { getToken } from "./auth";
+import qs from 'qs';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:16666/',//服务器端接口
+    baseURL: 'http://localhost:33333/',//本地服务器端接口—测试用
+    //baseURL: 'http://121.89.192.99:33333', //服务器端部署测试
     timeout: 5000
 })
 
@@ -10,11 +12,20 @@ const instance = axios.create({
 instance.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
     config.headers["authorization"] = "Bearer" + getToken();
-    return config;
+
+    if (config.type == 'formData' || config.method != 'post') {
+        return config
+    }
+    config.data = qs.stringify(config.data)
+    return config
+    //return config;
 }, function (error) {
     // 对请求错误做些什么
     return Promise.reject(error);
 });
+
+
+
 
 // 添加响应拦截器
 instance.interceptors.response.use(function (response) {
@@ -24,12 +35,12 @@ instance.interceptors.response.use(function (response) {
     // 对响应错误做点什么
     return Promise.reject(error);
 });
-
+//请求发送后执行
 export function get(url, params) { //params是url传参，查询条件
     return instance.get(url, { params })
 }
 
-export function post(url, data) {
+export function post(url, data) { //data数据，url请求地址
     return instance.post(url, data)
 }
 
