474f60b444c323ea01b66cf1f8c802e029829b55
Valleyard <50876390+Valleyard@users.noreply.github.com>
2020-07-04 10:25:20 +0800

    7-4更新

--- react-daoyun/src/pages/admin/params/Edit.js
+++ react-daoyun/src/pages/admin/params/Edit.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Card, Input, Button, Select, message } from 'antd';
-import { insertParam, getParamById, modifyParamById } from '../../../utils/data';
+import { set,getParamById, modifyParamById } from '../../../utils/data';
+import { modifyParamApi,getParamApi } from '../../../services/params';
+import { setToken, getToken } from '../../../utils/auth';
 
+
+const paramsList = 'paramsList';
 function Edit(props) {
     console.log(props);
     const [currentData, setCurrentData] = useState({});
@@ -12,8 +16,8 @@ function Edit(props) {
             console.log(data);
             setCurrentData(data);
             form.setFieldsValue({
-                Chinese: data.Chinese,
-                English: data.English,
+                chinese: data.chinese,
+                english: data.english,
                 value: data.value
             });
         }
@@ -34,21 +38,54 @@ function Edit(props) {
 
     const onFinish = values => {
 
-        if (props.match.params.id) {
-            if (modifyParamById(props.match.params.id, values)) {
-                message.success('修改成功！');
-                props.history.push('/admin/params');
-            } else {
-                message.error('修改失败！');
+        if (props.match.params.id) {  
+            var experence,distance=0;
+            if(props.match.params.id==1){
+                experence=values.value;
+                var data1 = getParamById(2);
+                distance=data1.value;
+            }else{
+                distance=values.value;
+                var data1 = getParamById(1);
+                experence=data1.value;
             }
-        } else {
-            if (insertParam(values)) {
-                message.success('添加参数成功！');
-                props.history.push('/admin/params');
-            } else {
-                message.error('该参数已存在！');
-            }
-        }
+            let token= getToken("token");
+            modifyParamApi(token, experence,distance).then((res)=>{
+                if(res.data.result_code === '200'){
+                    let token= getToken("token");
+                    let param_list=[];
+                    getParamApi(token).then((res)=>{
+                        if(res.data.result_code === '200'){
+                            console.log("success get params list!");
+                            for (let i = 0; i < res.data.data.length; i++) {
+                                let dataInfo1 = {
+                                    id: 1,
+                                    chinese:"经验值",
+                                    english: "experience",
+                                    value: res.data.data[i].experience
+                                };
+                                let dataInfo2 = {
+                                    id: 2,
+                                    chinese:"距离",
+                                    english: "distance",
+                                    value: res.data.data[i].distance
+                                };
+                                param_list.push(dataInfo1);
+                                param_list.push(dataInfo2);
+                                set(paramsList, param_list); 
+                                props.history.push('/admin/params');  
+                            }
+                        }
+                        else if(res.data.result_code === '206'){
+                            console.log("token time out!");
+                            message.error("token time out!");
+                        }else{
+                            console.log(res.data);
+                        }
+                    })                 
+                }
+            })
+        } 
     };
 
     return (
@@ -64,13 +101,13 @@ function Edit(props) {
             }>
             <Form {...layout} name="paramsEdit" onFinish={onFinish} validateMessages={validateMessages}
                 form={form}>
-                <Form.Item name="Chinese" label="中文" rules={[{ required: true }]}>
-                    <Input />
+                <Form.Item name="chinese" label="中文" rules={[{ required: true }]}>
+                    <Input disabled={true}/>
                 </Form.Item>
-                <Form.Item name="English" label="英文" rules={[{ required: true }]}>
-                    <Input />
+                <Form.Item name="english" label="英文" rules={[{ required: true }]}>
+                    <Input disabled={true}/>
                 </Form.Item>
-                <Form.Item name="value" label="默认值" rules={[{ required: true }]}>
+                <Form.Item name="value" label="值" rules={[{ required: true }]}>
                     <Input />
                 </Form.Item>
 
--- react-daoyun/src/pages/admin/params/List.js
+++ react-daoyun/src/pages/admin/params/List.js
@@ -1,14 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { Card, Table, Button, Popconfirm, message } from 'antd';
-import { initParams, delParamById } from '../../../utils/data';
+import { get,set,initParams } from '../../../utils/data';
+import { setToken, getToken } from '../../../utils/auth';
+import { getParamApi } from '../../../services/params';
+
+const paramsList = 'paramsList';
+export function getParams(){
+    let token= getToken("token");
+    // set(paramsList, []);
+    let param_list=[];
+    getParamApi(token).then((res)=>{
+        if(res.data.result_code === '200'){
+            console.log("success get params list!");
+            for (let i = 0; i < res.data.data.length; i++) {
+                let dataInfo1 = {
+                    id: 1,
+                    chinese:"经验值",
+                    english: "experience",
+                    value: res.data.data[i].experience
+                };
+                let dataInfo2 = {
+                    id: 2,
+                    chinese:"距离",
+                    english: "distance",
+                    value: res.data.data[i].distance
+                };
+                param_list.push(dataInfo1);
+                param_list.push(dataInfo2);
+                set(paramsList, param_list);   
+            }
+        }
+        else if(res.data.result_code === '206'){
+            console.log("token time out!");
+            message.error("token time out!");
+        }else{
+            console.log(res.data);
+        }
+    })
+}
 
 function List(props) {
     const [dataSource, setDataSource] = useState([]);
     useEffect(() => {
+        getParams();
         var data = initParams();
-        console.log(data);
+        console.log("params"+data[0].value);
         setDataSource(data);
     }, []);
+    
     const columns = [{
         title: '序号',
         key: 'id',
@@ -17,12 +56,12 @@ function List(props) {
         render: (txt, record, index) => index + 1
     }, {
         title: '中文',
-        dataIndex: 'Chinese'
-    }, {
+        dataIndex: 'chinese'
+    },{
         title: '英文',
-        dataIndex: 'English'
+        dataIndex: 'english'
     }, {
-        title: '默认值',
+        title: '值',
         dataIndex: 'value'
     }, {
         title: '操作',
